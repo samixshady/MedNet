79f707e9e93ec563e381581088a18e2bfddca1b1
Sarwar Sami <samixshady22@gmail.com>
2025-12-20 19:30:01 +0600

    Latest

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-admin-layout>
     <x-slot name="header">
         <div class="flex items-center justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -46,4 +46,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-admin-layout>
